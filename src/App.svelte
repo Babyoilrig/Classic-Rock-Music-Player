d76b7a7961c8cc5618fbf388a655cc2c17f26431
Babyoilrig <93347177+Babyoilrig@users.noreply.github.com>
2022-02-10 12:20:43 +0000

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,47 @@
+<script>
+import {onMount} from "svelte";
+import {musicList} from "./musiclist.js';
+let currentSongIndex = 0;
+let playerState = "play";
+let audioElement;
+let mainElement;
+</script>
+
+<style>
+	
+</style>
+
+<main bind:this={mainElement} >
+	<audio
+		src={"./files/audio/" +$musicList[currentSongIndex].audio}
+		bind:this={audioElement}
+		autoplay="false"
+	>
+	</audio>
+	<div class="player">
+		<div class="current-song">
+			<div class="avatar">
+				<img src={$musicList[currentSongIndex].image}>
+			</div>
+				<div class="song-controls">
+					<h2>{$musicList[currentSongIndex].name}</h2>
+					<div class="controls">
+						<button on:click={prev}>
+							<i class="fa fa-backward"></i>
+						</button>
+						<button on:click={playpause}>
+							{#if playerState == "play"}
+								<i class="fa fa-pause"></i>
+							{:else}
+								<i class="fa fa-play"></i>
+							{/if }
+						</button>
+						<button on:click={next}>
+							<i class="fa fa-forward"></i>
+						</button>
+					</div>
+				</div>
+		</div>
+	</div>
+</main>
+
---
+++ src/musiclist.js
@@ -0,0 +1,28 @@
+import {writable} from "svelte/store";
+
+export const musiclist = writable([
+{   image: "acdc-back-in-black.jpeg",
+    audio: "AC_DC - Back In Black (Official Video) (128kbit_AAC).m4a",
+    name: "Back in black",
+    artist: "ACDC"
+},
+{   image: "motorhead-ace-of-spades.jpeg",
+    audio: "Motorhead - Ace Of Spades + Lyrics (HQ) (128kbit_AAC).m4a",
+    name: "Ace of Spades",
+    artist:
+},
+{
+    image: "black-sabbath-the-wizard.jpeg",
+    audio:"Black Sabbath _The Wizard_ (128kbit_AAC).m4a",
+    name: "The Wizard",
+    artist: "Black Sabbath"
+},
+{
+    image: "guns-n-roses-night-train.jpeg",
+    audio: "Guns and Roses - Night train (128kbit_AAC).m4a",
+    name: "Night Train", 
+    artist: "Guns and Roses"
+}
+//Might need a comma after line 25?
+]
+);
