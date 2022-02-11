49a5874496e38b24adfd768dcf075fc85185f697
Babyoilrig <93347177+Babyoilrig@users.noreply.github.com>
2022-02-11 16:33:00 +0000

    Added more songs

--- src/musiclist.js
+++ src/musiclist.js
@@ -24,6 +24,44 @@ export const musicList = writable([
     name: "Night Train", 
     artist: "Guns and Roses"
 },
+{
+    image: "def-leppard-animal.jpeg",
+    audio: "Def Leppard - Animal (Official Lyric Video) (128kbit_AAC).m4a",
+    name: "Animal", 
+    artist: "Def Leppard"
+},
+{
+    image: "joan-jett-i-love-rock-n-roll.jpeg",
+    audio: "Joan Jett I Love Rock n_ Roll_ Lyrics (128kbit_AAC).m4a",
+    name: "Joan Jett", 
+    artist: "I love rock n roll"
+},
+{
+    image: "motley-crue-kickstart-my-heart.jpeg",
+    audio: "Mötley Crüe - Kickstart my Heart (128kbit_AAC).m4a",
+    name: "Motley Crue", 
+    artist: "Kickstart my heart"
+},
+{
+    image: "rainbow-since-youve-been-gone.jpeg",
+    audio: "Rainbow - Since you_ve been gone with lyrics (128kbit_AAC).m4a",
+    name: "Since you've been gone", 
+    artist: "Rainbow"
+},
+{
+    image: "queen-dont-stop-me-now.jpeg",
+    audio: "Queen - Dont Stop Me Now (Lyrics). (128kbit_AAC).m4a",
+    name: "Don't stop me now", 
+    artist: "Queen"
+},
+
+//To add new entries
+// {
+//     image: "",
+//     audio: "",
+//     name: "", 
+//     artist: ""
+// },
 
 ]
 );
